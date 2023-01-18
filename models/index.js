097d21562b15a58ba6611ab26d51f915e60dcf8b
Geeraldo Jose <geraldo.lantang@nadihealth.com>
2023-01-18 20:05:18 +0800

    ssl reconfig

--- models/index.js
+++ models/index.js
@@ -14,7 +14,7 @@ dotenv.config()
 
 
 const { PGHOST, PGDATABASE, PGUSER, PGPASSWORD, ENDPOINT_ID } = process.env;
-const URL = `postgres://${PGUSER}:${PGPASSWORD}@${PGHOST}/${PGDATABASE}?options=project%3D${ENDPOINT_ID}`;
+const URL = `postgres://${PGUSER}:${PGPASSWORD}@${PGHOST}/${PGDATABASE}?options=project%3D${ENDPOINT_ID}?ssl=true`;
 const sql = postgres(URL, { ssl: 'require' });
 console.log(sql);
 
